7d0105bd8e4a15db57313fd56834414b778251ac
SilviuShader <silviu.stancioiu@gmail.com>
2020-03-30 10:15:35 +0300

    redesigned the UI

--- js/transitions.js
+++ js/transitions.js
@@ -1,30 +1,44 @@
-const PagesEnum = {"Home" : 0, "Exam" : 1, "Edit" : 2, "EditEntry" : 3};
+const PagesEnum = {
+    "Home": 0,
+    "Exam": 1,
+    "Edit": 2,
+    "EditEntry": 3
+};
 
-var currentPage = null;
+let currentPage = null;
 
-function switchPage(page)
-{
+function switchPage(page) {
     currentPage = page;
-    var home      = document.getElementsByClassName("home")[0];
-    var exam      = document.getElementsByClassName("exam-page")[0];
-    var edit      = document.getElementsByClassName("edit-list-page")[0];
-    var editEntry = document.getElementsByClassName("edit-entry-page")[0];
+    let home = document.getElementsByClassName("home")[0];
+    let exam = document.getElementsByClassName("exam-page")[0];
+    let edit = document.getElementsByClassName("edit-list-page")[0];
+    let editEntry = document.getElementsByClassName("edit-entry-page")[0];
 
-    home.style.display      = "none";
-    exam.style.display      = "none";
-    edit.style.display      = "none";
+    home.style.display = "none";
+    exam.style.display = "none";
+    edit.style.display = "none";
     editEntry.style.display = "none";
 
-    switch (currentPage)
-    {
+    let homeTopButtons = document.getElementById("home-top-buttons");
+    let examTopButtons = document.getElementById("exam-top-buttons");
+    let editTopButtons = document.getElementById("edit-top-buttons");
+
+    homeTopButtons.style.display = "none";
+    examTopButtons.style.display = "none";
+    editTopButtons.style.display = "none";
+
+    switch (currentPage) {
         case PagesEnum.Home:
-            home.style.display      = "grid";
-        break;
+            home.style.display = "grid";
+            homeTopButtons.style.display = "block";
+            break;
         case PagesEnum.Exam:
-            exam.style.display      = "block";
+            exam.style.display = "block";
+            examTopButtons.style.display = "block";
             break;
         case PagesEnum.Edit:
-            edit.style.display      = "flex";
+            edit.style.display = "flex";
+            editTopButtons.style.display = "block";
             break;
         case PagesEnum.EditEntry:
             editEntry.style.display = "flex";
